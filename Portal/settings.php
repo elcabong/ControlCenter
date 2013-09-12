447fec2d62d0772772b60ef671376cec7024c6c9
elcabong <mbarone000@gmail.com>
2013-09-11 19:24:16 -0700

    test

--- Portal/settings.php
+++ Portal/settings.php
@@ -153,6 +153,7 @@ if ($authsecured && (!isset($_SESSION["$authusername"]) || !$_SESSION["$authuser
                     $date = $github->getInfo();
 					if(isset($branchname)) {
 					   echo $date['name']; //['author']['date'];
+					   echo $date['commit']['1']['date'];
 					} else {
                        echo $date['pushed_at'];
 					}
