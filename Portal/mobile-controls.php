372442cb02cfad84c7f080047d18b144b959291a
elcabong <mbarone000@gmail.com>
2013-09-21 12:00:01 -0700

    updated new settings for mobile site.

--- Portal/mobile-controls.php
+++ Portal/mobile-controls.php
@@ -48,10 +48,10 @@ while(!$found1){
 	<div id='nav-menu2'>
 		<nav id="navsettings" style="float:right;width:50px;">
 			<ul>
-				<li><a href='#Settings' class='navsettings panel' title='Settings' style="margin-bottom:3px;border-bottom:2px solid rgba(0, 0, 0, 0);"><img src="../media/gear.png" style="margin:7px 0 0;width:20px !important;"></a>
+				<li><a href='#SettingsL' class='navsettings panel' title='Settings' style="margin-bottom:3px;border-bottom:2px solid rgba(0, 0, 0, 0);"><img src="../media/gear.png" style="margin:7px 0 0;width:20px !important;"></a>
 					<ul>
 						<li><a href="#" class="title"><?echo $USERNAMES[$usernumber];?></a></li>
-						<li><a href='#Settings' class='panel2nd' title='Settings'>Settings</a></li>
+						<?//<li><a href='#Settings' class='panel2nd' title='Settings'>Settings</a></li>?>
 						<li><a href="#">&nbsp;</a></li>
 						<li><a href='logout.php' />Logout</a></li>
 					</ul>
@@ -59,9 +59,8 @@ while(!$found1){
 			</ul>
 		</nav>
 		<li id="loading" style="padding:10px;"><img src="../media/loading.gif" height='25px'></li>
-		<? /*<li><a href='#maraschinoadmin' class='panel unloaded' title='maraschinoadmin'><img src="../media/Programs/MaraschinoAdmin.png" height='35px'></a></li> */?>
-		<li><a href='#XBMC' class='panel selected' title='XBMC'><img src="../media/Programs/XBMC.png" height='35px'></a></li>
-		<li><a href='#XBMCawxi' class='panel unloaded' title='XBMCawxi'><img src="../media/Programs/XBMC.png" height='35px'></a></li>
+		<li><a href='#ROOMCONTROL1' class='panel selected' title='ROOMCONTROL1'><img src="../media/Programs/XBMC.png" height='35px'></a></li>
+		<li><a href='#ROOMCONTROL2' class='panel unloaded' title='ROOMCONTROL2'><img src="../media/Programs/XBMC.png" height='35px'></a></li>
 			<?php
 			$c = 1;
 			$count = 0;
@@ -136,76 +135,66 @@ while(!$found1){
 						if($navgroups =='1'){ echo "</ul>";}	
 			} else {
 				echo "<ul class='sortable'>";
-				$navgroups = '0';
-				$tempc = 0;
-				if($gnavlinkcount + $navlinkcount > 1) { $navgroups = '1';}
-				if(!empty($gnavlink)) {
-					foreach( $gnavlink as $navlinklabel => $navlinkpath) {
-					if($navlinklabel == "MaraschinoAdmin") {
-						if($navlinkpath == "title") {
-							if($navgroups == '1'){
-								$filename = "../media/Programs/".$navlinklabel.".png";
+				try {
+					$sql = "SELECT navgroup,navgrouptitle FROM navigation ORDER BY navgrouptitle ASC";
+					$navgroupamt = 0;
+					foreach ($configdb->query($sql) as $row)
+						{
+							$navgroup = $row['navgroup'];
+							if(strpos($NAVGROUPS,$navgroup) !== false) {
+								if($row['navgrouptitle'] == "1") { $navgroupamt++; }
+							}
+						}
+				} catch(PDOException $e)
+					{
+					echo $e->getMessage();
+					}
+				try {
+					$sql = "SELECT * FROM navigation ORDER BY navgroup ASC";
+					$tempc = 0;
+					foreach ($configdb->query($sql) as $row)
+						{
+						$navtitle = $row['navname'];
+						if(isset($row['navip'])) { $navdestination = $row['navip']; }
+						$navgroup = $row['navgroup'];
+						$navgrouptitle = $row['navgrouptitle'];
+						$mobiledestination = 0;
+						if(isset($row['mobile']) && $row['mobile'] != '') { $mobiledestination = $row['mobile']; }
+						if($mobiledestination != '0') {
+							if($mobiledestination != "1") { $navdestination = $mobiledestination; }
+							if(strpos($NAVGROUPS,$navgroup) !== false) {
+								if($navgrouptitle == "1") {
+									if($navgroupamt > '1'){
+										$filename = "../media/Programs/".$navtitle.".png";
+										if (file_exists($filename)) {
+											$linkto = "<img src=$filename height='35px' title='$navtitle'>";
+										} else {
+											$linkto = $navtitle;
+										}
+										$tempc++;
+										if($tempc>'1'){
+											echo "</li>";
+											echo "<li id=".$tempc." class='sortable secondary clear hidden'><a href='#' class='main panel title' title='$navtitle'>".$linkto."</a>";
+										} else {
+											echo "<li id=".$tempc." class='sortable clear'><a href='#' class='main panel title' title='$navtitle'>".$linkto."</a>";
+										}
+									}
+								} else {
+								$filename = "../media/Programs/".$navtitle.".png";
 								if (file_exists($filename)) {
-									$linkto = "<img src=$filename height='35px' title='$navlinklabel'>";
+									$linkto = "<img src=$filename height='35px' title='$navtitle'>";
 								} else {
-									$linkto = $navlinklabel;
+									$linkto = $navtitle;
 								}
-								$tempc++;
-								if($tempc>1){
-									echo "</li>";
-									echo "<li id=".$tempc." class='sortable secondary clear hidden'><a href='#' class='main panel title' title='$navlinklabel'>".$linkto."</a>";
-								} else {
-									echo "<li id=".$tempc." class='sortable clear'><a href='#' class='main panel title' title='$navlinklabel'>".$linkto."</a>";
+								echo "<a href='#".$navtitle."' class='main panel unloaded' title='$navtitle'>".$linkto."</a>";
 								}
 							}
-						} else {
-						$filename = "../media/Programs/".$navlinklabel.".png";
-						if (file_exists($filename)) {
-							$linkto = "<img src=$filename height='35px' title='$navlinklabel'>";
-						} else {
-							$linkto = $navlinklabel;
+						}	
 						}
-						echo "<a href='#".$navlinklabel."' class='main panel unloaded' title='$navlinklabel'>".$linkto."</a>";
-						}
-					}
-				}} /*
-				if(!empty($navlink)){
-						foreach( $navlink as $navlinklabel => $navlinkpath) {
-						if($navlinkpath == "title") {
-							if($navgroups == '1'){
-								$filename = "../media/Programs/".$navlinklabel.".png";
-								if (file_exists($filename)) {
-									$linkto = "<img src=$filename height='35px' title='$navlinklabel'>";
-								} else {
-									$linkto = $navlinklabel;
-								}
-								$tempc++;
-								if($tempc>1){
-									echo "</li>";
-									echo "<li id=".$tempc." class='sortable secondary clear hidden'><a href='#' class='main panel title' title='$navlinklabel'>".$linkto."</a>";
-								} else {
-									echo "<li id=".$tempc." class='sortable clear'><a href='#' class='main panel title' title='$navlinklabel'>".$linkto."</a>";
-								}
-							}
-						} else {
-						$filename = "../media/Programs/".$navlinklabel.".png";
-						if (file_exists($filename)) {
-							$linkto = "<img src=$filename height='35px' title='$navlinklabel'>";
-						} else {
-							$linkto = $navlinklabel;
-						}
-						echo "<a href='#".$navlinklabel."' class='main panel unloaded' title='$navlinklabel'>".$linkto."</a>";
-						}
-					}
-				}
-*/
-
-
-
-
-
-
-
+				} catch(PDOException $e)
+					{
+					echo $e->getMessage();
+					}				
 			}
 		?>
 		</ul>
@@ -216,50 +205,53 @@ while(!$found1){
 <div class="clearcover" style="position:absolute;width:100%;top:50px;bottom:0;display:none;background-color:rgba(0,0,0,.30);z-index:150;"></div>
 <div id="wrapper" scrolling="auto">
 	<div id="mask">
-		<? /*<div id="maraschinoadmin" class="item">
+		<div id="ROOMCONTROL1" class="item">
 			<div class="content">
-				<iframe id='maraschinoadminf' src="http://192.168.3.217:7000/mobile/" width='100%' height='100%' scrolling='auto'>Sorry your browser does not support frames or is currently not set to accept them.</iframe>
-		</div>
-		</div> */?>
-		<div id="XBMC" class="item">
-			<div class="content">
-				<?php $ROOMXT = "ROOM$theroom"; $XBMC = "XBMC"; $ROOMXBMC = $ROOMXT.$XBMC; ?>
-				<iframe id='XBMCf' class='XBMC' src="<?echo ${$ROOMXBMC};?>" width='100%' height='100%' scrolling='no'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>
+				<?php $ROOMXT = "ROOM$theroom"; $XBMC = "XBMC"; $ROOMXBMC = $ROOMXT.$XBMC; $ROOMXBMC2 = $ROOMXBMC."2"; ?>
+				<iframe id='ROOMCONTROL1f' class='ROOMCONTROL1' src="<?echo ${$ROOMXBMC};?>" width='100%' height='100%' scrolling='no'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>
 			</div>
 		</div>
-		<div id="XBMCawxi" class="item">
+		<div id="ROOMCONTROL2" class="item">
 			<div class="content">
-				<iframe id='XBMCawxif' class='XBMCawxi' data-src="<?echo ${$ROOMXBMC};?>/addons/webinterface.jquerymobile/" width='100%' height='100%' scrolling='auto'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>
+				<iframe id='ROOMCONTROL2f' class='ROOMCONTROL2' data-src="<?echo ${$ROOMXBMC2};?>" width='100%' height='100%' scrolling='auto'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>
 			</div>
 		</div>
-		<?php 
-		if(!empty($gnavlink)) {
-			foreach( $gnavlink as $navlinklabel => $navlinkpath) {
-				if($navlinkpath != "title") {
-					if($navlinklabel == "MaraschinoAdmin") {
-						echo "<div id='$navlinklabel' class='item'>";
-						echo "<div class='content'>";
-						echo "<iframe id='".$navlinklabel."f' class='$navlinklabel' data-src='".$navlinkpath."mobile/' width='100%' height='100%' scrolling='auto'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>";
-						echo "</div>";
-						echo "</div>";
-			}}}
-		}  /*
-			if(!empty($navlink)){
-				foreach( $navlink as $navlinklabel => $navlinkpath) {
-					if($navlinkpath != "title") {
-					echo "<div id='$navlinklabel' class='item'>";
-					echo "<div class='content'>";
-			       	 	echo "<iframe id='".$navlinklabel."f' class='$navlinklabel' data-src='".$navlinkpath."' width='100%' height='100%' scrolling='auto'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>";
-					echo "</div>";
-					echo "</div>";		
-				} }
-			}
-	*/	?>
-	<!--	<div id="Settings" class="item">
+		<?php
+				try {
+					$sql = "SELECT * FROM navigation ORDER BY navgroup ASC";
+					$tempc = 0;
+					foreach ($configdb->query($sql) as $row)
+						{
+						$navtitle = $row['navname'];
+						if(isset($row['navip'])) { $navdestination = $row['navip']; }
+						$navgroup = $row['navgroup'];
+						$navgrouptitle = $row['navgrouptitle'];
+						$mobiledestination = 0;
+						if(isset($row['mobile']) && $row['mobile'] != '') { $mobiledestination = $row['mobile']; }
+						if($mobiledestination != '0') {
+							if($mobiledestination != "1") { $navdestination = $mobiledestination; }
+							if(strpos($NAVGROUPS,$navgroup) !== false) {
+								if($navgrouptitle == "1") {
+								} else {
+									echo "<div id='$navtitle' class='item'>";
+									echo "<div class='content'>";
+										echo "<iframe id='".$navtitle."f' class='$navtitle' data-src='".$navdestination."' width='100%' height='100%' scrolling='auto'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>";
+									echo "</div>";
+									echo "</div>";
+								}
+							}
+						}
+						}
+				} catch(PDOException $e)
+					{
+					echo $e->getMessage();
+					}
+		?>
+	<?/*	<div id="Settings" class="item">
 			<div class="content">
 				<iframe id='Settingsf' src='./settings.php' width='100%' height='100%' scrolling='no'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>
 			</div>
-		</div>	-->	
+		</div>	*/?>
 	</div>
 </div>
 </body>
