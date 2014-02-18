6876f7475230924ed26ef2bdffab7c0e9f0c3dbc
elcabong <mbarone000@gmail.com>
2014-02-18 14:01:42 -0800

    addon setting addition

--- addons/service.pingmachine/addonquicklink.php
+++ addons/service.pingmachine/addonquicklink.php
@@ -1,3 +1,6 @@
 <?php
-		echo "<li><a href='#PINGMACHINE1' class='panel persistent unloaded'><img src='$ADDONDIR/service.pingmachine/media/XBMC.png' height='35px'></a></li>";
+		$setting1 = $enabledaddonsarray["$roomid"]['service.pingmachine']['setting1'];
+		if($setting1 == '1') { 
+			echo "<li><a href='#PINGMACHINE1' class='panel persistent unloaded'><img src='$ADDONDIR/service.pingmachine/media/XBMC.png' height='35px'></a></li>";
+			}
 ?>
--- addons/service.pingmachine/settings.php
+++ addons/service.pingmachine/settings.php
@@ -16,5 +16,6 @@ $setting10 = $enabledaddonsarray[$roomid]['service.pingmachine']['setting10'];
 
 										echo "<tr><td>&nbsp;</td></tr><tr><td></td><td colspan=5>$title</td></tr>
 										 <tr><td class='title'>IP</td><td colspan=5><input size='80' name='ip' value='$ADDONIP'></td></tr>
-										 <tr><td class='title'>MAC</td><td colspan=5><input size='80' name='mac' value='$ADDONMAC'></td></tr>";
+										 <tr><td class='title'>MAC</td><td colspan=5><input size='80' name='mac' value='$ADDONMAC'></td></tr>
+										 <tr><td class='title'>Link</td><td colspan=5><input size='80' name='setting1' value='$setting1'></td></tr>";
 ?>
