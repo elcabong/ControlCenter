2edba679e0f42a503b9398d0aa2835fe2f35a905
elcabong <mbarone000@gmail.com>
2014-02-18 21:52:50 -0800

    x

--- addons/mediaplayer.xbmc/addonlinkpages.php
+++ addons/mediaplayer.xbmc/addonlinkpages.php
@@ -3,14 +3,14 @@
 	$setting1 = $enabledaddonsarray["$roomid"]['mediaplayer.xbmc']['setting1'];
 		echo "<div id='XBMCCONTROL1' class='item'>
 			<div class='content'>
-				<iframe id='XBMCCONTROL1f' class='XBMCCONTROL1' src=\"$ADDONIP\" width='100%' height='100%' scrolling='no'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>
+				<iframe id='XBMCCONTROL1f' src=\"$ADDONIP\" width='100%' height='100%' scrolling='no'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>
 			</div>
 		</div>";
-		//if($enabledaddonsarray["$roomid"]['mediaplayer.xbmc']['setting1'] != '') {
+		if($enabledaddonsarray["$roomid"]['mediaplayer.xbmc']['setting1'] != '') {
 			echo"<div id='XBMCCONTROL2' class='item'>
 						<div class='content'>
-							<iframe id='XBMCCONTROL2f' class='XBMCCONTROL2' data-src=\"$setting1\" width='100%' height='100%' scrolling='auto'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>
+							<iframe id='XBMCCONTROL2f' data-src=\"$setting1\" width='100%' height='100%' scrolling='auto'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>
 						</div>
 					</div>";
-		//}			
+		}			
 ?>
--- addons/service.pingmachine/addonlinkpages.php
+++ addons/service.pingmachine/addonlinkpages.php
@@ -4,7 +4,7 @@
 		if($setting1 == '1') { 	
 		echo "<div id='PINGMACHINE1' class='item'>
 			<div class='content'>
-				<iframe id='PINGMACHINE1f' class='PINGMACHINE1' src=\"$ADDONIP\" width='100%' height='100%' scrolling='no'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>
+				<iframe id='PINGMACHINE1f' src=\"$ADDONIP\" width='100%' height='100%' scrolling='no'> Sorry your browser does not support frames or is currently not set to accept them.</iframe>
 			</div>
 		</div>";
 		}
