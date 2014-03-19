34ee59226541d07f9ffa0d12fc54d400d77d1753
elcabong <mbarone000@gmail.com>
2014-03-19 15:14:29 -0700

    a

--- Portal/wakemachine.php
+++ Portal/wakemachine.php
@@ -2,16 +2,19 @@
 if(isset($_GET['mac'])) {
 	$mac = $_GET['mac'];
 }
+if(isset($_GET['ip'])) {
+	$ip = $_GET['ip'];
+}
 echo "<script type='text/javascript' src='../js/jquery-1.10.1.min.js'></script>
 			<div style='color:#eee;margin: 5% auto 0;position: relative;text-align: center;'>
 			<h1>Power On</h1>
-			<a href='#' class='pingicon' onclick=\"wakethismachine('$mac');\" style='display: block;'>
+			<a href='#' class='pingicon' onclick=\"wakethismachine('$mac','$ip');\" style='display: block;'>
 			
 			<img src='../media/powerbutton-red.png' title='offline - click to try to wake machine' style='max-width:90%;max-height:40%;'/></a>
 			</div>";
 ?>
 <script>
-	function wakethismachine(mac) {
+	function wakethismachine(mac,ip) {
 		$.ajax({
 			   type: "POST",
 			   url: "wol-check.php?m="+mac+"",
@@ -19,7 +22,7 @@ echo "<script type='text/javascript' src='../js/jquery-1.10.1.min.js'></script>
 			   cache: false,
 			   success: function(response)
 			{
-				alert(response);
+				parent.updateroom();	
 		   }
 		});
 	}
